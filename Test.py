b48663b5d98b719cbd2d4df4b0336ad2d849d8af
Mariia <mariia.petrova@sdworx.com>
2024-10-08 17:44:44 +0300

    first code

---
+++ Test.py
@@ -0,0 +1,32 @@
+# test comments
+"""
+
+infinity list of comments
+
+name = input('Enter a name: ')
+birth_year = input('Enter bday ')
+
+current_year = 2024
+print(current_year - int(birth_year))
+
+print(1 + 2)
+print('I love ' + 'Python')
+print({0}, 'I love Python')
+
+"""
+'''
+test = (list(range(0, 500, 3)))
+
+for index, value in enumerate(test):
+    val2 = value % 15
+    if (val2 < 10):
+        print(pow(index, (val2)))
+
+'''
+
+print('remainder', 378 % 111) # 45
+print('floor division', 378 // 13) #29
+print('exp', 3 ** 5) #243
+
+print(2 in list(range(0, 4, 1)))
+print('land' in 'Finland')
