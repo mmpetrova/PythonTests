0f60b197111dd151a6b4bc2cbe42dd4dad1ce655
Mariia <mariia.petrova@sdworx.com>
2024-10-22 21:48:13 +0300

    last

--- Week04/some.py
+++ Week04/some.py
@@ -1,4 +1,4 @@
-
+'''
 sentence = 'I love people. People are great'
 words = sentence.split()
 print(sentence.swapcase())
@@ -7,3 +7,20 @@ print(words)
 print('love' in sentence)
 print(sentence.isalpha())
 print(sentence.isalnum())
+print('The slash (\\) allow us \\ to escape the default behavior')
+'''
+
+fnam = 'testfname'
+lnam = 'testlname'
+llang = 'Python'
+age = 100
+height = 200
+
+formated_string = 'I am {} {}. I teach {}. I am {} years old. I am {} meters tall. '.format(fnam, lnam, llang, age, height)
+
+print(formated_string)
+a, b, c, d, e, f = llang #unpacking sequence characters into variables
+print(a, b, c, d, e, f)
+print(llang[::-1])
+print(llang[0:6:2])
+
